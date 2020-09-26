939376d7eb7cd70c5896cb5436d58535e80e7286
Amitercse <amiter.cs@gmail.com>
2020-09-26 18:34:17 +0530

    Committing program to find duplicate in array

---
+++ Array/ArrayBasics/src/com/amit/array/DuplicateNumFinder.java
@@ -0,0 +1,98 @@
+package com.amit.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author amit
+ * Program to find duplicate number in the array.
+ * Assuming that there is only one duplicate in the array.
+ */
+public class DuplicateNumFinder {
+	
+	/**
+	 * Brute force approach to find duplicate number in array.
+	 * @author amit
+	 * @param arr
+	 */
+	public void findDuplicateNum(int[] arr) {
+		boolean foundDuplicate = false;
+		int duplicateNum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					duplicateNum = arr[i];
+					foundDuplicate = true;
+					break;
+				}
+			}
+		}
+		if (foundDuplicate) {
+			System.out.println("found duplicate number using brute force: " + duplicateNum);
+		} else {
+			System.out.println("No duplicate found");
+		}
+	}
+
+	/**
+	 * Found duplicate num in array after sorting elements.
+	 * It can be found in one iteration but needs extra complexity
+	 * to sort the array. 
+	 * @author amit
+	 * @param arr
+	 */
+	public void findDuplicateNumUsingSort(int[] arr) {
+		boolean duplicateFound = false;
+		int duplicateNum = 0;
+		Arrays.sort(arr);
+		int previous = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == previous) {
+				duplicateFound = true;
+				duplicateNum = arr[i];
+				break;
+			}
+			previous = arr[i];
+		}
+		if (duplicateFound) {
+			System.out.println("Duplicate found using sort: " + duplicateNum);
+		} else {
+			System.out.println("No duplicate found");
+		}
+	}
+	
+	/**
+	 * Find duplicate element using set. It needs O(n) complexity but 
+	 * also needs extra space of hashSet.
+	 * @author amit
+	 * @param arr
+	 */
+	public void findDuplicateUsingSet(int[] arr) {
+		int duplicateNum = 0;
+		boolean foundDuplicate = false;
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(arr[0]);
+		for (int i = 1; i < arr.length; i++) {
+			if (set.contains(arr[i])) {
+				foundDuplicate = true;
+				duplicateNum = arr[i];
+				break;
+			}
+			set.add(arr[i]);
+		}
+		if (foundDuplicate) {
+			System.out.println("found duplicate using set: " + duplicateNum);
+		} else {
+			System.out.println("no duplicate found");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 2, 5, 4, 7, 2 };
+		DuplicateNumFinder duplicateFinder = new DuplicateNumFinder();
+		duplicateFinder.findDuplicateNum(arr);
+		duplicateFinder.findDuplicateNumUsingSort(arr);
+		duplicateFinder.findDuplicateUsingSet(arr);
+	}
+}
