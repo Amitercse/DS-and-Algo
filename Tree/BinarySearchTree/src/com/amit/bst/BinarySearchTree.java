500c98a9d9620bb0daf9afeae8a846b58307fe20
Amitercse <amiter.cs@gmail.com>
2020-10-23 22:17:10 +0530

    Fixed minor issue in bst delete

--- Tree/BinarySearchTree/src/com/amit/bst/BinarySearchTree.java
+++ Tree/BinarySearchTree/src/com/amit/bst/BinarySearchTree.java
@@ -69,71 +69,65 @@ public class BinarySearchTree {
 		return isFound;
 	}
 	
-	public void deleteNode(int data)
-	{
-		if(root== null)
-		{
+	/**
+	 * Delete the node from binary search tree. Here while deleting we are trying to
+	 * maintain the property of BST.
+	 * @author amit
+	 * @param data
+	 */
+	public void deleteNode(int data) {
+		if (root == null) {
 			System.out.println("Tree is emplty");
-		}
-		else
-		{
-			BinaryTreeNode parent= null;
-			BinaryTreeNode currentNode= root;
-			while(currentNode!= null && currentNode.getData()!= data)
-			{
-				parent= currentNode;
-				if(data<currentNode.getData())
-				{
-					currentNode= currentNode.getLeft();
-				}
-				else
-				{
-					currentNode= currentNode.getRight();
+		} else {
+			BinaryTreeNode parent = null;
+			BinaryTreeNode currentNode = root;
+			// Find the node which needs to be deleted.
+			while (currentNode != null && currentNode.getData() != data) {
+				parent = currentNode;
+				if (data < currentNode.getData()) {
+					currentNode = currentNode.getLeft();
+				} else {
+					currentNode = currentNode.getRight();
 				}
 			}
-			if(currentNode== null)
-			{
+			if (currentNode == null) {
 				System.out.println("node not found");
-			}
-			else if(parent== null)
-			{
-				root= null;
+			} else if (parent == null && currentNode.getLeft() == null && currentNode.getRight() == null) {
+				root = null;
 				System.out.println("deleted root");
 			}
-			else if(currentNode.getLeft()== null && currentNode.getRight()== null)
-			{
-				if(parent.getLeft()== currentNode)
-				{
+			// If node needs to be deleted is leaf node.
+			else if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+				if (parent.getLeft() == currentNode) {
 					parent.setLeft(null);
-				}
-				else
-				{
+				} else {
 					parent.setRight(null);
 				}
 			}
-			else if(currentNode.getLeft()== null || currentNode.getRight()== null)
-			{
-				BinaryTreeNode node= currentNode.getLeft()!= null?currentNode.getLeft():currentNode.getRight();
-				if(parent.getLeft()== currentNode)
-				{
+			/*
+			 * Node needs to be deleted is having one child only, either left child or right
+			 * child. Here checking for || condition because if both the children are null
+			 * then previous if block would have caught it and control will never come here.
+			 */
+			else if (currentNode.getLeft() == null || currentNode.getRight() == null) {
+				BinaryTreeNode node = currentNode.getLeft() != null ? currentNode.getLeft() : currentNode.getRight();
+				if (parent.getLeft() == currentNode) {
 					parent.setLeft(node);
-				}
-				else
-				{
+				} else {
 					parent.setRight(node);
 				}
 			}
-			else
-			{
-				BinaryTreeNode node= currentNode.getRight();
-				if(parent.getLeft()== currentNode)
-				{
+			// Node needs to be deleted has both left and right child.
+			else {
+				BinaryTreeNode node = currentNode.getRight();
+				if (parent.getLeft() == currentNode) {
 					parent.setLeft(node);
-					node.setLeft(currentNode.getLeft());
-				}
-				else
-				{
+				} else {
 					parent.setRight(node);
+				}
+				if (node.getLeft() != null) {
+					node.getLeft().setLeft(currentNode.getLeft());
+				} else {
 					node.setLeft(currentNode.getLeft());
 				}
 			}
@@ -167,14 +161,21 @@ public class BinarySearchTree {
 	
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert(4);
-		bst.insert(7);
-		bst.insert(6);
-		bst.insert(3);
-		bst.insert(1);
 		bst.insert(8);
+		bst.insert(4);
+		bst.insert(12);
+		bst.insert(10);
+		bst.insert(14);
+		bst.insert(11);
+		bst.insert(9);
+		bst.insert(15);
+		bst.insert(13);
+		bst.insert(5);
+		bst.insert(3);
 		bst.traverseTree(bst.root);
 		boolean isFound = bst.searchItem(4);
 		System.out.println("\nelement found: " + isFound);
+		bst.deleteNode(12);
+		bst.traverseTree(bst.root);
 	}
 }
