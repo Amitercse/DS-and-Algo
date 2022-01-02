b822cc7534280e21aff0f8c526976d17fdf22804
Amit Sharma <amiter.cs@gmail.com>
2022-01-02 12:52:58 +0530

    union and intersection of list

--- Linked list/LinkedListBasicOperations/src/com/amit/list/ListBasicOperations.java
+++ Linked list/LinkedListBasicOperations/src/com/amit/list/ListBasicOperations.java
@@ -1,49 +1,64 @@
 package com.amit.list;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ListBasicOperations {
 
-	private Node head;
+	private static Node head;
 	
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4,5};
+		int arr[]= {1,2,3,4,5,1};
 		ListBasicOperations operations= new ListBasicOperations();
-		operations.buildLinkedList(arr);
+		Node headNode= operations.buildLinkedList(arr);
+		head= headNode;
 		operations.insertAfter(6, 3);
 		operations.deleteByData(2);
-		operations.displayElements();
+		operations.displayElements(head);
 		boolean dataFound= operations.searchData(4);
 		System.out.println("data found: "+ dataFound);
 	//	operations.addLoop();
 		boolean loopDetected= operations.detectLoop();
 		System.out.println("Loop detected: "+ loopDetected);
+		operations.findMid();
+		operations.removeDuplicates();
+		operations.displayElements(head);
+		Node firstListHead= head;
+		int newArr[]= {7,8,3,9,10,4};
+		Node secondListHead= operations.buildLinkedList(newArr);
+	//	operations.findUnion(firstListHead, secondListHead);
+		operations.findIntersection(firstListHead, secondListHead);
 		operations.reverseLinkedList();
-		operations.displayElements();
+		operations.displayElements(head);
 	}
 	
 	/**
 	 * Build a linked list from array
 	 */
-	private void buildLinkedList(int[] arr) {
+	private Node buildLinkedList(int[] arr) {
+		Node headNode= null;
 		for (int i : arr) {
-			insertAtEnd(i);
+			headNode= insertAtEnd(i, headNode);
 		}
+		return headNode;
 	}
 	
 	/**
 	 * Insert at the end of linked list
 	 */
-	private void insertAtEnd(int data) {
+	private Node insertAtEnd(int data, Node headNode) {
 		Node node = new Node(data);
 		node.setNext(null);
-		if (head == null) {
-			head = node;
+		if (headNode == null) {
+			headNode = node;
 		} else {
-			Node currentNode = head;
+			Node currentNode = headNode;
 			while (currentNode.getNext() != null) {
 				currentNode = currentNode.getNext();
 			}
 			currentNode.setNext(node);
 		}
+		return headNode;
 	}
 	
 	/**
@@ -185,11 +200,140 @@ public class ListBasicOperations {
 		}
 		currentNode.setNext(head);
 	}
+	
+	/**
+	 * Find mid of a linked list
+	 */
+	private void findMid() {
+		if (head == null) {
+			System.out.println("list is empty");
+			return;
+		}
+		if (head.getNext() == null) {
+			System.out.println("list has only one element");
+			return;
+		}
+		Node slow = head;
+		Node fast = head;
+		while (fast != null && fast.getNext() != null) {
+			slow = slow.getNext();
+			fast = fast.getNext().getNext();
+		}
+		System.out.println("Mid data: " + slow.getData());
+	}
+
+	/**
+	 * Remove duplicates from linked list
+	 */
+	private void removeDuplicates() {
+		if (head == null) {
+			return;
+		}
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(head.getData());
+		Node previousNode = head;
+		Node currentNode = head.getNext();
+		while (currentNode != null) {
+			if (set.contains(currentNode.getData())) {
+				previousNode.setNext(currentNode.getNext());
+				currentNode = currentNode.getNext();
+			} else {
+				previousNode = currentNode;
+				currentNode = currentNode.getNext();
+			}
+		}
+	}
+	
+	private void findUnion(Node firstListHead, Node secondListHead) {
+		if (firstListHead == null && secondListHead == null) {
+			return;
+		}
+		Node unionHead= null;
+		Node currentNode = firstListHead;
+		Node unionNode = null;
+		Node unionPreviousNode = null;
+		Set<Integer> set = new HashSet<>();
+		while (currentNode != null) {
+			if (unionNode == null) {
+				unionNode = currentNode;
+				currentNode = currentNode.getNext();
+				set.add(unionNode.getData());
+			} else if (!set.contains(currentNode.getData())) {
+				unionPreviousNode = unionNode;
+				unionNode = currentNode;
+				unionPreviousNode.setNext(unionNode);
+				currentNode = currentNode.getNext();
+				set.add(unionNode.getData());
+			} else {
+				currentNode = currentNode.getNext();
+			}
+		}
+		currentNode = secondListHead;
+		while (currentNode != null) {
+			if (unionNode == null) {
+				unionNode = currentNode;
+				currentNode = currentNode.getNext();
+				set.add(unionNode.getData());
+			} else if (!set.contains(currentNode.getData())) {
+				unionPreviousNode = unionNode;
+				unionNode = currentNode;
+				unionPreviousNode.setNext(unionNode);
+				currentNode = currentNode.getNext();
+				set.add(unionNode.getData());
+			} else {
+				currentNode = currentNode.getNext();
+			}
+		}
+		unionHead= firstListHead;
+		if(firstListHead== null)
+		{
+			unionHead= secondListHead;
+		}
+		displayElements(unionHead);
+	}
+	
+	private void findIntersection(Node firstListHead, Node secondListHead) {
+		if (firstListHead == null && secondListHead == null) {
+			return;
+		}
+		Node unionHead = null;
+		Node currentNode = firstListHead;
+		Node unionNode = null;
+		Node unionPreviousNode = null;
+		Set<Integer> set = new HashSet<>();
+		while (currentNode != null) {
+			set.add(currentNode.getData());
+			currentNode = currentNode.getNext();
+		}
+		currentNode = secondListHead;
+		while (currentNode != null) {
+			if (set.contains(currentNode.getData())) {
+				if (unionNode == null) {
+					unionNode = new Node(currentNode.getData());
+					unionHead= unionNode;
+					currentNode = currentNode.getNext();
+					set.add(unionNode.getData());
+				} else {
+					unionPreviousNode = unionNode;
+					unionNode = new Node(currentNode.getData());
+					unionPreviousNode.setNext(unionNode);
+					currentNode = currentNode.getNext();
+					set.add(unionNode.getData());
+				}
+			} else {
+				set.add(currentNode.getData());
+				currentNode = currentNode.getNext();
+			}
+		}
+		displayElements(unionHead);
+	}
+
+
 
 	/**
 	 * Display elements of a linked list
 	 */
-	private void displayElements() {
+	private void displayElements(Node head) {
 		Node currentNode = head;
 		while (currentNode != null) {
 			System.out.print(currentNode.getData() + "\t");
