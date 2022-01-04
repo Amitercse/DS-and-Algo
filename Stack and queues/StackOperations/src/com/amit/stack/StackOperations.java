310c4147d31d0259abcc27579c0fd7690465ade0
Amit Sharma <amiter.cs@gmail.com>
2022-01-04 08:31:36 +0530

    postfix evaluation

--- Stack and queues/StackOperations/src/com/amit/stack/StackOperations.java
+++ Stack and queues/StackOperations/src/com/amit/stack/StackOperations.java
@@ -2,7 +2,7 @@ package com.amit.stack;
 
 public class StackOperations {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		StackOperations operations= new StackOperations();
 		Stack stack = new Stack(6);
 		stack.peek();
@@ -25,6 +25,8 @@ public class StackOperations {
 		System.out.println(newStack.pop());
 		System.out.println(newStack.pop());
 		System.out.println(newStack.pop());
+		int result= operations.evaluatePostFix("921*-8-4+");
+		System.out.println("postfix result: "+ result);
 	}
 	
 	/**
@@ -50,6 +52,45 @@ public class StackOperations {
 			}
 		}
 		return newStack;
+	}
+	
+	/**
+	 * Evaluate postfix expression
+	 * @param expression
+	 * @return
+	 * @throws Exception 
+	 */
+	private int evaluatePostFix(String expression) throws Exception {
+		if (expression == null || expression.isEmpty()) {
+			System.out.println("expression is blank");
+			return -1000; // dummy number to show that expression is incorrect
+		}
+		Stack stack = new Stack(expression.length());
+		for (int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+			if (!Character.isDigit(c)) {
+				int a = stack.pop();
+				int b = stack.pop();
+				if (c == '+') {
+					stack.push(b + a);
+				}
+				if (c == '-') {
+					stack.push(b - a);
+				}
+				if (c == '*') {
+					stack.push(a * b);
+				}
+				if (c == '/') {
+					if (a == 0) {
+						throw new Exception("divide by 0 is not possible");
+					}
+					stack.push(b / a);
+				}
+			} else {
+				stack.push(Character.getNumericValue(c));
+			}
+		}
+		return stack.pop();
 	}
 
 }
