f570261ca19267cebac19f0ccc7245a73b17b8d2
Amit Sharma <amiter.cs@gmail.com>
2022-01-09 17:29:25 +0530

    graph related operations

--- Graph/GraphBasics/src/com/amit/operations/GraphOperations.java
+++ Graph/GraphBasics/src/com/amit/operations/GraphOperations.java
@@ -1,7 +1,11 @@
 package com.amit.operations;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.amit.model.DoublyLinkedList;
 import com.amit.model.Graph;
+import com.amit.model.Queue;
 
 public class GraphOperations {
 
@@ -15,6 +19,23 @@ public class GraphOperations {
 			}
 		}
 		return false;
+	}
+	
+	public int countEdgesinUndirectedGraph(Graph g) {
+		int size = g.getVertices();
+		DoublyLinkedList[] adjacencyList = g.getAdjacencyList();
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			DoublyLinkedList.Node temp = null;
+			if (adjacencyList[i] != null) {
+				temp = adjacencyList[i].getHead();
+			}
+			while (temp != null) {
+				count++;
+				temp = temp.next;
+			}
+		}
+		return count / 2;
 	}
 
 	private boolean cycle(Graph g, int sourceVertex, boolean[] visited, boolean[] recursiveVisit) {
@@ -41,5 +62,61 @@ public class GraphOperations {
 		recursiveVisit[sourceVertex] = false;
 		return false;
 	}
-
+	
+	public boolean isTree(Graph g, int rootVertex) {
+		Queue queue = new Queue(g.getVertices());
+		Set<Integer> visited = new HashSet<Integer>();
+		queue.enqueue(rootVertex);
+		visited.add(rootVertex);
+		int visitedCount = 1;
+		while (!queue.isEmpty()) {
+			int vertex = queue.dequeue();
+			DoublyLinkedList.Node temp = null;
+			if (g.getAdjacencyList()[vertex] != null) {
+				temp = g.getAdjacencyList()[vertex].getHead();
+			}
+			while (temp != null) {
+				if (visited.contains(temp.data)) {
+					return false;
+				}
+				queue.enqueue(temp.data);
+				visited.add(temp.data);
+				visitedCount++;
+				temp = temp.next;
+			}
+		}
+		return (visitedCount != g.getVertices());
+	}
+	
+	public int shortestPathBetweenVertices(Graph g, int source, int destination) {
+		int size = g.getVertices();
+		Queue queue = new Queue(size);
+		Set<Integer> visited = new HashSet<>();
+		int[] distance = new int[size];
+		if (source == destination) {
+			return 0;
+		}
+		queue.enqueue(source);
+		visited.add(source);
+		distance[source] = 0;
+		while (!queue.isEmpty()) {
+			int vertex = queue.dequeue();
+			DoublyLinkedList.Node temp = null;
+			if (g.getAdjacencyList()[vertex] != null) {
+				temp = g.getAdjacencyList()[vertex].getHead();
+			}
+			while (temp != null) {
+				if (!visited.contains(temp.data)) {
+					visited.add(temp.data);
+					queue.enqueue(temp.data);
+					distance[temp.data] = distance[vertex] + 1;
+				}
+				if (temp.data == destination) {
+					return distance[temp.data];
+				}
+				temp = temp.next;
+			}
+		}
+		return -1;
+	}
 }
--- Graph/GraphBasics/src/com/amit/operations/Main.java
+++ Graph/GraphBasics/src/com/amit/operations/Main.java
@@ -19,5 +19,27 @@ public class Main {
 		GraphOperations op= new GraphOperations();
 		boolean isCyclePresent=op.detectCycle(graph);
 		System.out.println("has cycle: "+ isCyclePresent);
+		Graph undirectedGraph= buildUndirectedGraph();
+		int edgeCount= op.countEdgesinUndirectedGraph(undirectedGraph);
+		System.out.println("edge count: "+ edgeCount);
+		System.out.println("is tree: "+ op.isTree(graph, 0));
+		System.out.println("distance between vertices: "+ op.shortestPathBetweenVertices(undirectedGraph, 0, 7));
+	}
+	
+	private static Graph buildUndirectedGraph()
+	{
+		Graph graph = new Graph(8);
+		graph.addEdgeUndirected(0, 1);
+		graph.addEdgeUndirected(0, 4);
+		graph.addEdgeUndirected(1, 2);
+		graph.addEdgeUndirected(1, 3);
+		graph.addEdgeUndirected(4, 2);
+		graph.addEdgeUndirected(4, 5);
+		graph.addEdgeUndirected(2, 5);
+		graph.addEdgeUndirected(5, 6);
+		graph.addEdgeUndirected(5, 7);
+		graph.addEdgeUndirected(5, 3);
+		graph.addEdgeUndirected(6, 7);
+		return graph;
 	}
 }
