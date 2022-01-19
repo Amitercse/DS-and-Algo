3cd25ec46a21bf29e234372c03b1ee616bf87b05
Amit Sharma <amiter.cs@gmail.com>
2022-01-19 21:57:42 +0530

    longest palindrome subsequence

---
+++ Dynamic-programming/Palindromes/src/com/amit/palindrome/LongestPalindromeSubseq.java
@@ -0,0 +1,48 @@
+package com.amit.palindrome;
+
+public class LongestPalindromeSubseq {
+
+	public static void main(String[] args) {
+		LongestPalindromeSubseq subseq= new LongestPalindromeSubseq();
+		System.out.println(subseq.longestPalindromeSubsequence("abdbca"));
+		
+	}
+
+	public int longestPalindromeSubsequence(String s) {
+		Integer[][] dp= new Integer[s.length()][s.length()];
+		// return recursiveLongestPalindromeSubsequence(s, 0, s.length() - 1);
+		return recursiveLongestPalindromeSubsequence(dp, s, 0, s.length()-1);
+	}
+
+	private int recursiveLongestPalindromeSubsequence(String s, int startIndex, int endIndex) {
+		if (startIndex > endIndex)
+			return 0;
+		if (startIndex == endIndex)
+			return 1;
+
+		if (s.charAt(startIndex) == s.charAt(endIndex))
+			return 2 + recursiveLongestPalindromeSubsequence(s, startIndex + 1, endIndex - 1);
+
+		int length1 = recursiveLongestPalindromeSubsequence(s, startIndex + 1, endIndex);
+		int length2 = recursiveLongestPalindromeSubsequence(s, startIndex, endIndex - 1);
+		return Math.max(length1, length2);
+	}
+
+	private int recursiveLongestPalindromeSubsequence(Integer[][] dp, String s, int startIndex, int endIndex) {
+		if (startIndex > endIndex)
+			return 0;
+		if (startIndex == endIndex)
+			return 1;
+
+		if (dp[startIndex][endIndex] == null) {
+			if (s.charAt(startIndex) == s.charAt(endIndex)) {
+				dp[startIndex][endIndex] = 2 + recursiveLongestPalindromeSubsequence(s, startIndex + 1, endIndex - 1);
+			} else {
+				int length1 = recursiveLongestPalindromeSubsequence(s, startIndex + 1, endIndex);
+				int length2 = recursiveLongestPalindromeSubsequence(s, startIndex, endIndex - 1);
+				dp[startIndex][endIndex] = Math.max(length1, length2);
+			}
+		}
+		return dp[startIndex][endIndex];
+	}
+}
